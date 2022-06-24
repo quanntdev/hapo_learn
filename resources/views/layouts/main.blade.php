c95a01eb33829a9f55f715d9a065af7c9d3a8826
NguyenTrongQuan <qquannguyentrong123@gmail.com>
2022-06-24 11:32:39 +0700

    [#3] create list items

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,89 @@
+<div class="main">
+    <div class="main-content">
+       <div class="list-items">
+          <div class="items">
+             <div class="img-items bg-3F6185">
+                <img src="{{ asset('images/html.png')}}" alt="">
+             </div>
+             <div class="content-items">
+                <p class="title">HTML/CSS/js turtorial</p>
+                <p class="content">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched anything in regards to web development.</p>
+                <p class="btn-link"> 
+                   <a href="#" >Take This Course</a>
+                </p>
+             </div>
+          </div>
+          <div class="items">
+             <div class="img-items bg-DC594C">
+                <img src="{{ asset('images/lavarel.png')}}" alt="">
+             </div>
+             <div class="content-items">
+                <p class="title">Laravel tutorial</p>
+                <p class="content">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched anything in regards to web development.</p>
+                <p class="btn-link"> 
+                   <a href="#" >Take This Course</a>
+                </p>
+             </div>
+          </div>
+          <div class="items">
+             <div class="img-items bg-556598">
+                <img src="{{ asset('images/php.png')}}" alt="">
+             </div>
+             <div class="content-items">
+                <p class="title">PHP turtorial</p>
+                <p class="content">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched anything in regards to web development.</p>
+                <p class="btn-link"> 
+                   <a href="#" >Take This Course</a>
+                </p>
+             </div>
+          </div>
+       </div>
+    </div>
+    <div class="main-content">
+       <p class="big-tittle">
+          Other courses
+       </p>
+       <div class="line-border"></div>
+       <div class="list-items no-tranfer mt-45">
+          <div class="items">
+             <div class="img-items bg-3F6185">
+                <img src="{{ asset('images/html.png')}}" alt="">
+             </div>
+             <div class="content-items">
+                <p class="title">HTML/CSS/js turtorial</p>
+                <p class="content">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched anything in regards to web development.</p>
+                <p class="btn-link"> 
+                   <a href="#" >Take This Course</a>
+                </p>
+             </div>
+          </div>
+          <div class="items">
+             <div class="img-items bg-DC594C">
+                <img src="{{ asset('images/lavarel.png')}}" alt="">
+             </div>
+             <div class="content-items">
+                <p class="title">Laravel tutorial</p>
+                <p class="content">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched anything in regards to web development.</p>
+                <p class="btn-link"> 
+                   <a href="#" >Take This Course</a>
+                </p>
+             </div>
+          </div>
+          <div class="items">
+             <div class="img-items bg-556598">
+                <img src="{{ asset('images/php.png')}}" alt="">
+             </div>
+             <div class="content-items">
+                <p class="title">PHP turtorial</p>
+                <p class="content">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched anything in regards to web development.</p>
+                <p class="btn-link"> 
+                   <a href="#" >Take This Course</a>
+                </p>
+             </div>
+          </div>
+       </div>
+       <div class="view-more-title">
+          <a href="">View All Our Courses <i class="fa-solid fa-arrow-right"></i></a> 
+       </div>
+    </div>
+ </div>
