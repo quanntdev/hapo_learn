50a2d76d3425a632a7e177948f9c61ab151a57ac
NguyenTrongQuan <qquannguyentrong123@gmail.com>
2022-06-28 16:59:53 +0700

    update css_2

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,42 +1,107 @@
 <footer class="text-center text-lg-start text-white pt-5">
-<div class="footer">
-    <section class="">
-        <div class=" text-center text-md-start ">
-            <div class="footer-content">
-                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                    <div class="footer-logo " href="#">
-                        <img  src="{{ asset('images/hapo_white.png') }}" alt="HapoLearn Logo">
+    <div class="footer">
+        <section class="">
+            <div class=" text-center text-md-start ">
+                <div class="footer-content">
+                    <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                        <div class="footer-logo " href="#">
+                            <img  src="{{ asset('images/hapo_white.png') }}" alt="HapoLearn Logo">
+                        </div>
+                        <p class="slogan-company">
+                            Interactive lessons, "on-the-go" practice, peer support
+                        </p>
                     </div>
-                    <p class="slogan-company">
-                        Interactive lessons, "on-the-go" practice, peer support
-                    </p>
+                    <div class="col-md-1 col-lg-1 col-xl-1 mx-auto">
+                        <p>
+                            <a href="#!" class="text-reset">Home</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">List course</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">List lesson</a>
+                        </p>
+                    </div>
+                    <div class="col-md-1 col-lg-1 col-xl-1 mx-auto p-0">
+                        <p>
+                            <a href="#!" class="text-reset">Lesson detail</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">Login/Register</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">Profile</a>
+                        </p>
+                    </div>
+                    <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 ">
+                        <div class="list-icon">
+                            <p class="icon-item">
+                                <a href=""><i class="fa-brands fa-facebook"></i></a>
+                            </p>
+                            <p class="icon-item">
+                                <a href=""><i class="fa-solid fa-envelope"></i></a>
+                            </p>
+                            <p class="icon-item">
+                                <a href=""><i class="fa-solid fa-phone"></i></a>
+                            </p>
+                        </div>
+                        <div class="list-address">
+                            <p>
+                                <a href="">
+                                facebook.com/tuyen.dung.haposoft
+                                </a>
+                            </p>
+                            <p>
+                                <a href="">
+                                info@haposoft.com
+                                </a>
+                            </p>
+                            <p>
+                                <a href="">
+                                +84-85-645-9898
+                                </a>
+                            </p>
+                        </div>
+                    </div>
                 </div>
-
-                <div class="col-md-1 col-lg-1 col-xl-1 mx-auto">
-                    <p>
-                        <a href="#!" class="text-reset">Home</a>
-                    </p>
-                    <p>
-                        <a href="#!" class="text-reset">List course</a>
-                    </p>
-                    <p>
-                        <a href="#!" class="text-reset">List lesson</a>
-                    </p>
+            </div>
+        </section>
+    </div>
+    <div class="footer-mobie">
+        <div class="footer-mobie-content">
+            <div class="w-footer">
+                <p>
+                    <a href="#!" class="text-reset">Home</a>
+                </p>
+                <p>
+                    <a href="#!" class="text-reset">List course</a>
+                </p>
+                <p>
+                    <a href="#!" class="text-reset">List lesson</a>
+                </p>
+            </div>
+            <div class="w-footer">
+                <p>
+                    <a href="#!" class="text-reset">Lesson detail</a>
+                </p>
+                <p>
+                    <a href="#!" class="text-reset">Login/Register</a>
+                </p>
+                <p>
+                    <a href="#!" class="text-reset">Profile</a>
+                </p>
+            </div>
+            <div class="w-footer">
+                <div class="footer-logo " href="#">
+                    <img  src="{{ asset('images/hapo_white.png') }}" alt="HapoLearn Logo">
                 </div>
-                <div class="col-md-1 col-lg-1 col-xl-1 mx-auto p-0">
-                    <p>
-                        <a href="#!" class="text-reset">Lesson detail</a>
-                    </p>
-                    <p>
-                        <a href="#!" class="text-reset">Login/Register</a>
-                    </p>
-                    <p>
-                        <a href="#!" class="text-reset">Profile</a>
-                    </p>
-                </div>
-                <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 ">
-                   <div class="list-icon">
-                     <p class="icon-item">
+                <p class="slogan-company">
+                    Interactive lessons, "on-the-go" practice, peer support
+                </p>
+            </div>
+            <div class="w-footer">
+                <div class="list-icon">
+                    <p class="icon-item">
                         <a href=""><i class="fa-brands fa-facebook"></i></a>
                     </p>
                     <p class="icon-item">
@@ -45,78 +110,12 @@
                     <p class="icon-item">
                         <a href=""><i class="fa-solid fa-phone"></i></a>
                     </p>
-                   </div>
-                   <div class="list-address">
-                    <p>
-                        <a href="">
-                            facebook.com/tuyen.dung.haposoft
-                        </a>
-                    </p>
-                    <p>
-                        <a href="">
-                            info@haposoft.com
-                        </a>
-                    </p>
-                    <p>
-                        <a href="">
-                            +84-85-645-9898
-                        </a>
-                    </p>
-                   </div>
                 </div>
             </div>
         </div>
-    </section>
-</div>
-<div class="footer-mobie">
-    <div class="footer-mobie-content">
-        <div class="w-footer">
-            <p>
-                <a href="#!" class="text-reset">Home</a>
-            </p>
-            <p>
-                <a href="#!" class="text-reset">List course</a>
-            </p>
-            <p>
-                <a href="#!" class="text-reset">List lesson</a>
-            </p>
-        </div>
-        <div class="w-footer">
-            <p>
-                <a href="#!" class="text-reset">Lesson detail</a>
-            </p>
-            <p>
-                <a href="#!" class="text-reset">Login/Register</a>
-            </p>
-            <p>
-                <a href="#!" class="text-reset">Profile</a>
-            </p>
-        </div>
-        <div class="w-footer">
-            <div class="footer-logo " href="#">
-                <img  src="{{ asset('images/hapo_white.png') }}" alt="HapoLearn Logo">
-            </div>
-            <p class="slogan-company">
-                Interactive lessons, "on-the-go" practice, peer support
-            </p>
-        </div>
-        <div class="w-footer">
-            <div class="list-icon">
-                <p class="icon-item">
-                   <a href=""><i class="fa-brands fa-facebook"></i></a>
-               </p>
-               <p class="icon-item">
-                   <a href=""><i class="fa-solid fa-envelope"></i></a>
-               </p>
-               <p class="icon-item">
-                   <a href=""><i class="fa-solid fa-phone"></i></a>
-               </p>
-            </div>
-        </div>
     </div>
-</div>
-<div class="clear"></div>
-<div class="copyright">
-     © 2020 HapoLearn, Inc. All rights reserved.
-</div>
+    <div class="clear"></div>
+    <div class="copyright">
+        © 2020 HapoLearn, Inc. All rights reserved.
+    </div>
 </footer>
