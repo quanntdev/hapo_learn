9e153532ff37b460548fc1573c1f53096f52287e
NguyenTrongQuan <qquannguyentrong123@gmail.com>
2022-06-29 12:15:26 +0700

    update scss

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,43 +1,43 @@
 <div class="main">
    <div class="main-content">
       <div class="list-items">
-          <div class="items">
-             <div class="img-items bg-3F6185">
-                <img src="{{ asset('images/html.png')}}" alt="">
-             </div>
-             <div class="content-items">
-                <p class="title">HTML/CSS/js turtorial</p>
-                <p class="content">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched anything in regards to web development.</p>
-                <p class="btn-link"> 
-                   <a href="#" >Take This Course</a>
-                </p>
-             </div>
-          </div>
-          <div class="items">
-             <div class="img-items bg-DC594C">
-                <img src="{{ asset('images/lavarel.png')}}" alt="">
-             </div>
-             <div class="content-items">
-                <p class="title">Laravel tutorial</p>
-                <p class="content">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched anything in regards to web development.</p>
-                <p class="btn-link"> 
-                   <a href="#" >Take This Course</a>
-                </p>
+         <div class="items">
+            <div class="img-items">
+               <img src="{{ asset('images/html.png')}}" alt="">
+            </div>
+            <div class="content-items">
+               <p class="title">HTML/CSS/js turtorial</p>
+               <p class="content">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched anything in regards to web development.</p>
+               <p class="btn-link"> 
+                  <a href="#" >Take This Course</a>
+               </p>
              </div>
           </div>
           <div class="items">
-             <div class="img-items bg-556598">
-                <img src="{{ asset('images/php.png')}}" alt="">
-             </div>
-             <div class="content-items">
-                <p class="title">PHP turtorial</p>
-                <p class="content">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched anything in regards to web development.</p>
-                <p class="btn-link"> 
-                   <a href="#" >Take This Course</a>
-                </p>
-             </div>
+            <div class="img-items">
+               <img src="{{ asset('images/lavarel.png')}}" alt="">
+            </div>
+            <div class="content-items">
+               <p class="title">Laravel tutorial</p>
+               <p class="content">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched anything in regards to web development.</p>
+                p class="btn-link"> 
+                  <a href="#" >Take This Course</a>
+               </p>
+            </div>
           </div>
-       </div>
+          <div class="items">
+            <div class="img-items">
+               <img src="{{ asset('images/php.png')}}" alt="">
+            </div>
+            <div class="content-items">
+               <p class="title">PHP turtorial</p>
+               <p class="content">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched anything in regards to web development.</p>
+               <p class="btn-link"> 
+                  <a href="#" >Take This Course</a>
+               </p>
+            </div>
+         </div>
+      </div>
    </div>
    <div class="main-content">
       <p class="big-tittle">
