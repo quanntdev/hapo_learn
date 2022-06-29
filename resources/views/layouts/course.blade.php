c501ef3d658eb0f413edee41256b97d8979046ea
NguyenTrongQuan <qquannguyentrong123@gmail.com>
2022-06-29 09:30:06 +0700

    sua loi convention

--- resources/views/layouts/course.blade.php
+++ resources/views/layouts/course.blade.php
@@ -1,10 +1,10 @@
 <div class="banner-startlearn" style="background-image: url('{{ asset('images/banner3.png')}}');">
-    <div class="banner-startlearn-content">
-       <p class="banner-startlearn-title">
-          Become a member of our growing community!
-       </p>
-       <p class="banner-startlearn-link">
-          <a href="">Start Learning Now!</a>
-       </p>
-    </div>
- </div>
+   <div class="banner-startlearn-content">
+      <p class="banner-startlearn-title">
+         Become a member of our growing community!
+      </p>
+      <p class="banner-startlearn-link">
+         <a href="">Start Learning Now!</a>
+      </p>
+   </div>
+</div>
