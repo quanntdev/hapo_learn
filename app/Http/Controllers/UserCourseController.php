bd9157def20a2a530902afa71b2c803cbbd9869b
NguyenTrongQuan <qquannguyentrong123@gmail.com>
2022-08-02 09:37:03 +0700

    fix v1: delete function not use

--- app/Http/Controllers/UserCourseController.php
+++ app/Http/Controllers/UserCourseController.php
@@ -14,22 +14,6 @@ class UserCourseController extends Controller
         $this->middleware('auth')->only('store', 'update');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(StoreCourseUserRequest $request)
     {
         $courseUser = UserCourse::create([
@@ -41,51 +25,11 @@ class UserCourseController extends Controller
         return redirect()->back();
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(StoreCourseUserRequest $request, $id)
     {
         $courseUser = UserCourse::where('user_id', auth()->user()->id)->where('course_id', $id)->first();
         $courseUser->status = config('course.end_course');
         $courseUser->save();
         return redirect()->back();
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
