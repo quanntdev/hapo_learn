7e9e5c5848f329387b5431e7891b32cfcc33626a
NguyenTrongQuan <qquannguyentrong123@gmail.com>
2022-08-10 13:43:41 +0700

    fix return true false

--- resources/views/components/course/link-lesson.blade.php
+++ resources/views/components/course/link-lesson.blade.php
@@ -1,6 +1,6 @@
 <div class="link-lesson">
     @if($key + ( (isset($data['page']) ? $data['page'] : 1) -1) * config('course.lesson_paginate') == 0 )
-        @if (($course->isJoined || $course->isFinished) && $lesson->IsFinished() == false)
+        @if (($course->isJoined || $course->isFinished) && !$lesson->IsFinished())
             <a href="{{ route('lessons.show', [$lesson->slug_lesson]) }}" class="btn-start-learn"> {{ __('course-detail.link_lesson') }} </a>
         @elseif ($lesson->IsFinished())
             <a href="{{ route('lessons.show', [$lesson->slug_lesson]) }}" class="btn btn-success"> complete </a>
@@ -8,7 +8,7 @@
             <div class="cant-learn"> {{ __('course-detail.link_lesson') }} </div>
         @endif
     @elseif ($key + ( (isset($data['page']) ? $data['page'] : 1) -1) * config('course.lesson_paginate') > 0 && $key + ( (isset($data['page']) ? $data['page'] : 1) -1) * config('course.lesson_paginate') < config('course.lesson_paginate') )
-        @if (($course->isJoined || $course->isFinished) && $lesson->IsFinished() == false && ($lessons[$key + ( (isset($data['page']) ? $data['page'] : 1) -1) * config('course.lesson_paginate') - 1]->IsFinished() == true))
+        @if (($course->isJoined || $course->isFinished) && !$lesson->IsFinished() && ($lessons[$key + ( (isset($data['page']) ? $data['page'] : 1) -1) * config('course.lesson_paginate') - 1]->IsFinished()))
             <a href="{{ route('lessons.show', [$lesson->slug_lesson]) }}" class="btn-start-learn"> {{ __('course-detail.link_lesson') }} </a>
         @elseif ($lesson->IsFinished())
             <a href="{{ route('lessons.show', [$lesson->slug_lesson]) }}" class="btn btn-success"> complete </a>
@@ -18,13 +18,13 @@
     @else
         @if (isset($data['learned']))
             @if ($key == 0)
-                @if ($lesson->IsFinished() == false)
+                @if (!$lesson->IsFinished())
                     <a href="{{ route('lessons.show', [$lesson->slug_lesson]) }}" class="btn-start-learn"> {{ __('course-detail.link_lesson') }} </a>
                 @else
                     <a href="{{ route('lessons.show', [$lesson->slug_lesson]) }}" class="btn btn-success"> complete </a>
                 @endif
             @elseif ($key > 0)
-                @if ($lesson->IsFinished() == false && ($lessons[$key- 1]->IsFinished == true))
+                @if (!$lesson->IsFinished() && ($lessons[$key - 1]->IsFinished()))
                     <a href="{{ route('lessons.show', [$lesson->slug_lesson]) }}" class="btn-start-learn"> {{ __('course-detail.link_lesson') }} </a>
                 @elseif ($lesson->IsFinished())
                     <a href="{{ route('lessons.show', [$lesson->slug_lesson]) }}" class="btn btn-success"> complete </a>
